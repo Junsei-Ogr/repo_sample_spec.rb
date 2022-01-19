1a2ccd9047e943de347fe62eaf211320e8ff4dcd
Junsei-Ogr <nk.fakeittekina@gmail.com>
2022-01-19 16:34:01 +0000

    first commit

---
+++ sample_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe port(22) do
+  it { should be_listening }
+end
+
+describe package('nginx'), :if => os[:family] == 'amazon' do
+ it { should be_installed }
+end
+
+describe port(80) do
+ it { should be_listening }
+end
+
+# Ruby2.6.3がインストールされていること
+describe command('ruby -v') do
+ its(:stdout) { should match /ruby 2\.6\.3+/ }
+end
